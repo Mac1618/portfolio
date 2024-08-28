f7d2be6180577522474a84c676bbc181e3f38145
Mac1618 <ma46099@gmail.com>
2024-08-28 19:53:06 +0800

    refactor: Implemented my github account on navbar and removed the unnecessary logos.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,10 @@
 import Sidebar from '../sidebar/Sidebar';
 
 const Navbar = () => {
+	const handleLink = (url) => {
+		return window.open(`${url}`, '_black');
+	};
+
 	return (
 		<nav className="navbar h-[100px] relative">
 			{/* Sidebar */}
@@ -9,18 +13,15 @@ const Navbar = () => {
 			<div className="wrapper relative px-[8%] w-full m-auto flex justify-between items-center h-full max-sm:flex-col max-sm:justify-center">
 				<span className="font-bold">MY PORTFOLIO</span>
 				<div className="social flex gap-5 max-sm:gap-10 max-sm:mt-3">
-					<a href="#">
-						<img src="/facebook.png" alt="facebook image" width={24} height={24} />
-					</a>
-					<a href="#">
-						<img src="/instagram.png" alt="instagram image" width={24} height={24} />
-					</a>
-					<a href="#">
-						<img src="/youtube.png" alt="youtube image" width={24} height={24} />
-					</a>
-					<a href="#">
-						<img src="/dribbble.png" alt="dribbble image" width={24} height={24} />
-					</a>
+					<div className="cursor-pointer" onClick={() => handleLink('https://github.com/Mac1618')}>
+						<img
+							src="/github.png"
+							alt="github logo"
+							className="bg-white rounded-full cursor-pointer"
+							width={30}
+							height={30}
+						/>
+					</div>
 				</div>
 			</div>
 		</nav>
